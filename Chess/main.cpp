ffec94abcaafaf8d8686bdc3f0f8be32c02005b6
Artem Babin <artem031201@yandex.ru>
2020-07-24 15:20:59 +0300

    Connected SFML library

--- Chess/main.cpp
+++ Chess/main.cpp
@@ -1,4 +1,19 @@
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
 int main()
 {
+	RenderWindow window(VideoMode(200, 200), "SFML Works!");
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+		window.display();
+	}
 	return 0;
 }
